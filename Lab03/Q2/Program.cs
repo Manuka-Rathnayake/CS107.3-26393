c26a8ca79f8d93f1f99007f01065a31cab54199e
Manuka Rathnayake <115450597+Manuka-Rathnayake@users.noreply.github.com>
2023-07-25 21:10:00 +0530

    lab3

---
+++ Lab03/Q2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int num, sum = 0, m;
+            Console.Write("Enter a number: ");
+            num = int.Parse(Console.ReadLine());
+            while (num > 0)
+            {
+               m = num % 10;
+               sum = sum + m;
+               num = num / 10;
+            }
+            Console.Write("Sum is= " + sum);
+
+            Console.ReadLine();
+          
+        }
+    }
+}
---
+++ Lab03/Q3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.Write("Enter a Number:");
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num; i += 2)
+            {
+                sum += i;
+            }
+            Console.Write("Sum of Odd numbers from 1 to is :" + sum);
+            Console.ReadLine();
+        }
+    }
+}
